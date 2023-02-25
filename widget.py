a8cf294ced718ef87d2d92c2a277b23b6b99e86b
Arka Barua <arka.barua83@gmail.com>
2023-02-25 19:56:47 +0600

    textBrowser done

--- widget.py
+++ widget.py
@@ -1,7 +1,5 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
 from ui_widget import Ui_Widget
-
 import re
 from zxcvbn import zxcvbn
 
@@ -24,9 +22,22 @@ class Widget(QWidget, Ui_Widget):
     def check_pass(self):
         name = self.UserNameInput.text().split()
         print(self.PassInput.text())
-        result = zxcvbn(self.PassInput.text(), user_inputs=name+passwords)
+        result = zxcvbn(self.PassInput.text(), user_inputs=name + passwords)
         self.score.setText(str(result['score']))
         self.guesses.setText(str(result['guesses']))
         self.tenH.setText(str(result['crack_times_display']['online_throttling_100_per_hour']))
         self.tenKS.setText(str(result['crack_times_display']['offline_slow_hashing_1e4_per_second']))
         self.tenBS.setText(str(result['crack_times_display']['offline_fast_hashing_1e10_per_second']))
+
+        #li = ['matched_word', 'rank', 'pattern', 'reversed', 'guesses', 'dictionary_name']
+        li = ['l33t','uppercase_variations','i','l33t_variations','base_guesses', 'guesses_log10','j']
+        result_str = ""
+        for key, value in result.items():
+            if key == 'sequence':
+                for value2 in value:
+                    for key2, value3 in value2.items():
+                        if key2 not in li:
+                            result_str += f"{key2}: {value3}\n"
+                    result_str += "-------------------------\n\n"
+
+        self.textBrowser.setPlainText(result_str)
